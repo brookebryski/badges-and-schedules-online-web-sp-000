edb34ca2770fc05debcc233855e31a8ce49bfa43
chic-state-0895 <brookeabryski@gmail.com>
2020-08-28 22:52:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+names = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+def badge_maker(names)
+  names.each do |name|
+    puts "Hello, my name is #{name}."
+end
+end
+
+def batch_badge_creator(names)
+
+end
+
+def assign_rooms
+end
